4e24d9621e4251248cdffeb33285d35bb0793874
Andrii Tsokur <tsokur@gmail.com>
2023-04-27 00:01:25 +0300

    Adds solved Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+refs = {
+	body: document.body,
+	btnStart: document.querySelector('[data-start]'),
+	btnStop: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', handleStart);
+refs.btnStop.addEventListener('click', handleStop);
+
+function handleStart() {
+	refs.btnStart.setAttribute('disabled', true);
+	refs.btnStart.setAttribute('style', 'opacity:0.5');
+
+	intervalId = setInterval(changeBodyColor, 1000);
+}
+
+function handleStop() {
+	clearInterval(intervalId);
+
+	refs.btnStart.removeAttribute('disabled');
+	refs.btnStart.removeAttribute('style');
+	// refs.body.removeAttribute('style');
+}
+
+function changeBodyColor() {
+	refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
