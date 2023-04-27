96c784fba6ea122f75185d7d13bd90e72b82003d
Andrii Tsokur <tsokur@gmail.com>
2023-04-27 09:31:48 +0300

    Fixes error in 01-color-switchers.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,11 @@
 import { handleStart, handleStop } from './01/btnOps';
 
-export default refs = {
+const refs = {
 	btnStart: document.querySelector('[data-start]'),
 	btnStop: document.querySelector('[data-stop]'),
 };
 
 refs.btnStart.addEventListener('click', handleStart);
 refs.btnStop.addEventListener('click', handleStop);
+
+export default refs;
